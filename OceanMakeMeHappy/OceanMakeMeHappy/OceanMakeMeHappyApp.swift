67a37f9ab932c12d7185177d8263b2c1b52ea27f
미니 <cow970814@naver.com>
2023-03-03 10:44:22 +0900

    feat : Start Project

---
+++ OceanMakeMeHappy/OceanMakeMeHappy/OceanMakeMeHappyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OceanMakeMeHappyApp.swift
+//  OceanMakeMeHappy
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+        
+
+import SwiftUI
+
+@main
+struct OceanMakeMeHappyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
