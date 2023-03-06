3638c72b5260bb14fd82c4df7f4a61420044ab5a
미니 <cow970814@naver.com>
2023-03-06 15:35:14 +0900

    refactor : Map View Model 타입 정리 및 assign Map View 수정

--- OceanMakeMeHappy/OceanMakeMeHappy/UI/Screens/MapView/VIew/MapView.swift
+++ OceanMakeMeHappy/OceanMakeMeHappy/UI/Screens/MapView/VIew/MapView.swift
@@ -8,15 +8,6 @@ import MapKit
 import SwiftUI
 
 struct MapView: View {
-//    enum Constant {
-//        static var basicRegion = MKCoordinateRegion(
-//            center: CLLocationCoordinate2D(latitude: 33.3642, longitude: 126.5313),
-//            span: MKCoordinateSpan(latitudeDelta: 0.9, longitudeDelta: 0.9)
-//        )
-//    }
-//    @State private var region = Constant.basicRegion
-//    let markers: [Beach] = [.init(location: .init(latitude: 33.4348090000, longitude: 126.9230210000))]
-    
     @StateObject private var viewModel: MapViewModel
     
     init(mapViewModel: MapViewModel) {
@@ -24,13 +15,10 @@ struct MapView: View {
     }
     
     var body: some View {
-        VStack {
-            EmptyView()
+        Map(coordinateRegion: $viewModel.region, annotationItems: viewModel.beachList) { beach in
+            MapPin(coordinate: beach.location)
         }
-//        Map(coordinateRegion: $region, annotationItems: markers) { marker in
-//            MapPin(coordinate: marker.location, tint: .blue)
-//        }
-//        .edgesIgnoringSafeArea(.all)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
--- OceanMakeMeHappy/OceanMakeMeHappy/UI/Screens/MapView/ViewModel/MapViewModel.swift
+++ OceanMakeMeHappy/OceanMakeMeHappy/UI/Screens/MapView/ViewModel/MapViewModel.swift
@@ -5,21 +5,36 @@
 //  Copyright (c) 2023 Minii All rights reserved.
 
 import Combine
+import MapKit
 
 final class MapViewModel: ObservableObject {
     private let webService: BeachService
+    
     private var subscribers = Set<AnyCancellable>()
+    @Published var region = Constant.defaultRegion
+    @Published var beachList = [Beach]()
     
     init(webService: BeachService) {
         self.webService = webService
         
         webService.load()
-            .sink { error in
-                print(error)
-            } receiveValue: { datas in
-                print(datas)
-            }
+            .replaceError(with: [])
+            .assign(to: \.beachList, on: self)
             .store(in: &subscribers)
 
+    }
+}
+
+extension MapViewModel {
+    enum Constant {
+        static var defaultRegion = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(
+                latitude: 33.3642,
+                longitude: 126.5313
+            ),
+            span: MKCoordinateSpan(
+                latitudeDelta: 0.7, longitudeDelta: 0.7
+            )
+        )
     }
 }
