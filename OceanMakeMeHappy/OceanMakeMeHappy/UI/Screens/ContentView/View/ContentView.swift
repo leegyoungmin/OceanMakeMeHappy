54ebb5a8194fbd536e05aae1123161b60e0efb82
미니 <cow970814@naver.com>
2023-03-06 14:08:04 +0900

    refactor : Rounded Tab Bar View Builder로 변경

--- OceanMakeMeHappy/OceanMakeMeHappy/UI/Screens/ContentView/View/ContentView.swift
+++ OceanMakeMeHappy/OceanMakeMeHappy/UI/Screens/ContentView/View/ContentView.swift
@@ -7,51 +7,23 @@
 import MapKit
 import SwiftUI
 
-
-
 struct ContentView: View {
     @StateObject private var viewModel: ContentViewModel
     
     init() {
         _viewModel = StateObject(wrappedValue: ContentViewModel())
     }
-    
+
     var body: some View {
         NavigationView {
             ZStack {
-                TabView(selection: $viewModel.selectedIndex) {
+                if viewModel.selectedIndex == 0 {
                     MapView()
-                        .tag(0)
-
+                } else if viewModel.selectedIndex == 1 {
                     Text("Example")
-                        .tag(1)
                 }
                 
-                VStack {
-                    Spacer()
-                    
-                    HStack {
-                        Spacer()
-                        
-                        ForEach(viewModel.tabBarItems.indices, id: \.self) { index in
-                            let item = viewModel.tabBarItems[index]
-                            
-                            Button {
-                                viewModel.selectedIndex = index
-                            } label: {
-                                Image(systemName: item.imageName)
-                            }
-                            .foregroundColor(viewModel.isSelectedIndex(with: index) ? .accentColor : .black)
-                        }
-                        
-                        Spacer()
-                    }
-                    .frame(maxWidth: 200)
-                    .frame(minHeight: 50)
-                    .background(Color.white)
-                    .cornerRadius(25)
-                    .shadow(radius: 5)
-                }
+                roundedTabBar()
             }
             .customNavigationBar(centerView: {
                 Image("IconImage")
@@ -59,6 +31,36 @@ struct ContentView: View {
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 50, height: 50)
             })
+        }
+    }
+}
+
+extension ContentView {
+    @ViewBuilder
+    private func roundedTabBar() -> some View {
+        VStack {
+            Spacer()
+            
+            HStack {
+                Spacer()
+                
+                ForEach(viewModel.tabBarItems.indices, id: \.self) { index in
+                    let item = viewModel.tabBarItems[index]
+                    
+                    Button {
+                        viewModel.selectedIndex = index
+                    } label: {
+                        Image(systemName: item.imageName)
+                    }
+                    .foregroundColor(viewModel.isSelectedIndex(with: index) ? .accentColor : .black)
+                }
+                
+                Spacer()
+            }
+            .frame(maxWidth: 200, minHeight: 50)
+            .background(Color.white)
+            .cornerRadius(25)
+            .shadow(radius: 5)
         }
     }
 }
