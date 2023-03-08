8e42466ed20216c4aa4bafdac42fb734840d4c0a
미니 <cow970814@naver.com>
2023-03-08 23:32:08 +0900

    refactor : Map Marker Reset 메서드 추가

--- OceanMakeMeHappy/OceanMakeMeHappy/UI/Screens/MapView/VIew/NaverMapView.swift
+++ OceanMakeMeHappy/OceanMakeMeHappy/UI/Screens/MapView/VIew/NaverMapView.swift
@@ -28,24 +28,36 @@ struct NaverMapView: UIViewRepresentable {
         return mapController
     }
     
+    private func resetRemainMarker(marker: NMFMarker) {
+        let mapView = marker.mapView
+        
+        mapViewModel.locations.filter { $0 != marker.position }.forEach {
+            guard let point = mapView?.projection.point(from: $0) else { return }
+            guard let markers = mapView?.pickAll(point, withTolerance: 2) as? [NMFMarker] else { return }
+            
+            for mark in markers {
+                mark.iconImage = NMF_MARKER_IMAGE_LIGHTBLUE
+                mark.width = 16
+                mark.height = 21
+            }
+        }
+    }
      
     private func touchedMarker(marker overlay: NMFOverlay) -> Bool {
         guard let marker = overlay as? NMFMarker else { return false }
+        
+        resetRemainMarker(marker: marker)
         
         if marker.iconImage == NMF_MARKER_IMAGE_LIGHTBLUE { // UnSelected -> Selected
             withAnimation {
                 marker.iconImage = NMF_MARKER_IMAGE_BLUE
                 marker.width *= 2
                 marker.height *= 2
-            }
-            return true
-        }
-        
-        if marker.iconImage == NMF_MARKER_IMAGE_BLUE { // Selected -> UnSelected
-            withAnimation {
-                marker.iconImage = NMF_MARKER_IMAGE_LIGHTBLUE
-                marker.width /= 2
-                marker.height /= 2
+                let cameraUpdate = NMFCameraUpdate(scrollTo: marker.position, zoomTo: 15)
+                cameraUpdate.reason = Int32(NMFMapChangedByControl)
+                cameraUpdate.animation = .fly
+                cameraUpdate.animationDuration = 1
+                marker.mapView?.moveCamera(cameraUpdate)
             }
             return true
         }
@@ -56,9 +68,11 @@ struct NaverMapView: UIViewRepresentable {
     func updateUIView(_ uiView: NMFNaverMapView, context: Context) { }
     
     class Coordinator: NSObject, NMFMapViewTouchDelegate, NMFMapViewCameraDelegate, NMFMapViewOptionDelegate {
-        func mapView(_ mapView: NMFMapView, didTap symbol: NMFSymbol) -> Bool {
-            print(symbol)
-            return true
+        func mapView(_ mapView: NMFMapView, didTapMap latlng: NMGLatLng, point: CGPoint) {
+            let cameraUpdate = NMFCameraUpdate(scrollTo: latlng, zoomTo: 15)
+            cameraUpdate.animation = .fly
+            cameraUpdate.animationDuration = 1
+            mapView.moveCamera(cameraUpdate)
         }
     }
     
@@ -76,15 +90,18 @@ extension NaverMapView {
         view.showCompass = false
         view.showScaleBar = false
         view.showLocationButton = false
-        
+        view.mapView.locale = "ko_KR"
         
         view.mapView.positionMode = .normal
         view.mapView.isNightModeEnabled = true
-        view.mapView.allowsZooming = false
-        view.mapView.symbolScale = .zero
         view.mapView.zoomLevel = 9
+        view.mapView.maxZoomLevel = 15
         view.mapView.allowsRotating = false
         view.mapView.logoInteractionEnabled = false
+        
+        view.mapView.setLayerGroup(NMF_LAYER_GROUP_BUILDING, isEnabled: false)
+        view.mapView.setLayerGroup(NMF_LAYER_GROUP_MOUNTAIN, isEnabled: true)
+        view.mapView.symbolScale = 1
         
         view.mapView.extent = NMGLatLngBounds(latLngs: mapViewModel.beachList.map { $0.location })
         
@@ -97,9 +114,13 @@ extension NaverMapView {
             let marker = NMFMarker(position: location)
             marker.width = 16
             marker.height = 21
+            marker.captionText = $0.name
             marker.mapView = mapController.mapView
             marker.iconImage = NMF_MARKER_IMAGE_LIGHTBLUE
             marker.userInfo = ["name": $0.name]
+            marker.isHideCollidedCaptions = true
+            marker.isHideCollidedMarkers = true
+            marker.isHideCollidedSymbols = true
             marker.touchHandler = touchedMarker(marker:)
         }
     }
