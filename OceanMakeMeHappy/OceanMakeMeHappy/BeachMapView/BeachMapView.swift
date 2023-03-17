bc46a33d85b298a99f4fa1a22f64cd61de4c5dd4
미니 <cow970814@naver.com>
2023-03-17 18:18:52 +0900

    refactor : Beach Map View 구성 변경

--- OceanMakeMeHappy/OceanMakeMeHappy/BeachMapView/BeachMapView.swift
+++ OceanMakeMeHappy/OceanMakeMeHappy/BeachMapView/BeachMapView.swift
@@ -20,17 +20,13 @@ struct BeachMapView: View {
     var body: some View {
         WithViewStore(store) { viewStore in
             ZStack {
-                IfLetStore(
-                    store.scope(
-                        state: \.mapStore,
-                        action: BeachMapStore.Action.mapStore
-                    )
-                ) { store in
-                    NaverMapView(store: store)
-                        .edgesIgnoringSafeArea(.all)
-                }
+                naverMapSection
                 
-                previewCardSection(store: viewStore)
+                VStack {
+                    Spacer()
+                    
+                    previewCardSection
+                }
             }
             .task {
                 viewStore.send(.onAppear)
@@ -45,33 +41,38 @@ struct BeachMapView_Previews: PreviewProvider {
     }
 }
 
-extension BeachMapView {
-    @ViewBuilder
-    func previewCardSection(store: ViewStore<BeachMapStore.State, BeachMapStore.Action>) -> some View {
-        VStack {
-            Spacer()
-            
-            ZStack {
-                IfLetStore(
-                    self.store.scope(
-                        state: \.selectedPreviewBeachState,
-                        action: BeachMapStore.Action.previewCardAction
+private extension BeachMapView {
+    var naverMapSection: some View {
+        IfLetStore(
+            self.store.scope(
+                state: \.mapStore,
+                action: BeachMapStore.Action.mapStore
+            )
+        ) { store in
+            NaverMapView(store: store)
+                .edgesIgnoringSafeArea(.all)
+        }
+    }
+    
+    var previewCardSection: some View {
+        IfLetStore(
+            self.store.scope(
+                state: \.selectedPreviewBeachState,
+                action: BeachMapStore.Action.previewCardAction
+            )
+        ) { store in
+            BeachPreviewCardView(store: store)
+                .shadow(
+                    color: Color.gray.opacity(0.3),
+                    radius: 10
+                )
+                .padding()
+                .transition(
+                    .asymmetric(
+                        insertion: .move(edge: .trailing),
+                        removal: .move(edge: .leading)
                     )
-                ) { store in
-                    BeachPreviewCardView(store: store)
-                        .shadow(
-                            color: Color.gray.opacity(0.3),
-                            radius: 10
-                        )
-                        .padding()
-                        .transition(
-                            .asymmetric(
-                                insertion: .move(edge: .trailing),
-                                removal: .move(edge: .leading)
-                            )
-                        )
-                }
-            }
+                )
         }
     }
 }
