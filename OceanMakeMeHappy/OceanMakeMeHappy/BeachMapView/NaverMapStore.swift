aaaba63507091508b4b867531a6c325586c23b17
미니 <cow970814@naver.com>
2023-03-17 14:12:57 +0900

    feat : NaverMap Store 타입 기본 구현

---
+++ OceanMakeMeHappy/OceanMakeMeHappy/BeachMapView/NaverMapStore.swift
@@ -0,0 +1,36 @@
+//
+//  NaverMapStore.swift
+//  OceanMakeMeHappy
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import ComposableArchitecture
+import NMapsMap
+
+struct NaverMapStore: ReducerProtocol {
+    struct State: Equatable {
+        let beachList: [Beach]
+        let locations: [NMGLatLng]
+        
+        init(beachList: [Beach]) {
+            self.beachList = beachList
+            self.locations = beachList.map(\.location)
+        }
+    }
+    
+    enum Action: Equatable {
+        case selectBeach(Int)
+    }
+    
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .selectBeach(let index):
+                return .none
+                
+            default:
+                return .none
+            }
+        }
+    }
+}
